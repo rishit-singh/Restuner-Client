b4f47a733f09b413ec9a9ead15c273625ee8999e
rishit-singh <rsrishitsingh@gmail.com>
2024-02-25 00:49:24 -0800

    Made the UI dark themed

--- src/app/Tuner.tsx
+++ src/app/Tuner.tsx
@@ -7,22 +7,20 @@ export default function Tuner()
     return (<>
         <div className="grid grid-cols-2">
             <div className="flex flex-col items-center p-12">
-                <div className="h-[500px] w-[650px] bg-[#D9D9D9] rounded-lg p-5 text-[#303030]">
+                <div className="h-[500px] w-[650px] bg-[#252525] rounded-lg p-5 text-[#D9D6D6] border-[#313131] border-2">
                     Output 
                 </div>
-                <div className="grid grid-cols-2 p-12 gap-64">
-                    <Button>
-                        Tune
-                    </Button>
+                <div className="grid grid-cols-2 p-12 gap-52">
+                    <Button variant={"secondary"}>Tune</Button>
                     
-                    <div className="flex flex-row gap-3 items-center">
+                    <div className="flex flex-row gap-3 items-center p-0">
                         <Label htmlFor="resume">Resume: </Label>
-                        <Button>Select a file</Button>
+                        <Button className="m-0" variant={"secondary"}>Select a file</Button>
                     </div>
                 </div>
             </div>
             <div className="flex flex-col items-center p-12">
-                <Textarea placeholder={"Enter a job description"} className="mt-[50px] h-[300px] w-[650px] bg-[#D9D9D9] rounded-lg p-5 text-[#303030]"/>
+                <Textarea placeholder={"Enter a job description"} className="mt-[50px] h-[300px] w-[650px] bg-[#3B3B3B] rounded-lg p-5 text-[#D9D6D6]"/>
             </div>
         </div>
     </>);    
